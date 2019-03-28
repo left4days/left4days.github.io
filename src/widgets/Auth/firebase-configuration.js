bd51d8be92b8ba4bac5d831dab21755e8e76f462
cevcode <kobernikev@gmail.com>
2019-03-28 11:05:14 +0100

    add vk auth not finished

--- src/widgets/Auth/firebase-configuration.js
+++ src/widgets/Auth/firebase-configuration.js
@@ -1,4 +1,13 @@
+import React from 'react';
 import firebase from 'firebase';
+import { Redirect } from 'react-router-dom';
+import { getAllUrlParams } from './helpers';
+
+const PROVIDERS = {
+    google: new firebase.auth.GoogleAuthProvider(),
+    facebook: new firebase.auth.FacebookAuthProvider(),
+    twitter: new firebase.auth.TwitterAuthProvider(),
+};
 
 export function registerUser(login, email, password, registerBy) {
     return firebase.auth().createUserWithEmailAndPassword(email, password);
@@ -14,4 +23,27 @@ export function signOutUser() {
 
 export function resetUser(email) {
     return firebase.auth().sendPasswordResetEmail(email);
+}
+
+export function signWithSocial(type) {
+    console.log(type);
+    if (type === 'vk') {
+        return signInWithVK();
+    }
+    firebase.auth().signInWithPopup(PROVIDERS[type]);
+}
+
+export function signInWithVK() {
+    const clientId = '6916700';
+    const redirectUri = 'localhost:3000';
+    const url = `https://oauth.vk.com/authorize?client_id=${clientId}&scope=email&redirect_uri=${redirectUri}&response_type=token`;
+
+    const newWin = window.open(url, 'vk-login', 'width=665,height=370');
+    newWin.onload = function() {
+        let hash = newWin.location.hash;
+        const { access_token } = getAllUrlParams(hash);
+        if (access_token) {
+            return firebase.auth().signInWithCustomToken(access_token);
+        }
+    };
 }
