6718093c8d7ae27539f5658d882491446e7fab7a
Rinword <ilyabelenko@mail.ru>
2019-04-02 23:54:58 +0300

    fix(AppState) hot fix get

--- src/modules/Admin/index.js
+++ src/modules/Admin/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 import firebase from 'firebase';
+import get from 'lodash/get';
 
 import { getFirebaseHeaderToken } from 'widgets/requestsHelpers';
 import { Button } from 'ui/Button';
